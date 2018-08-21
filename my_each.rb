a5d25b5561f6911ace66141a0ef74bc0018db9d6
Sam Ashtar <samashtarm@gmail.com>
2018-08-21 20:08:24 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+
+def my_each(collection)
+  counter = 0
+  while collection.length > counter
+    yield(collection[counter])
+    counter += 1
+  end
+  collection
 end
